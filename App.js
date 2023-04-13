2506a352c8d7c45a41246808a600551cc8e8daa8
Anderson Santos <adnnskt@gmail.com>
2023-04-12 21:02:30 -0300

    add bottom menu

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
     </View>
   );
 }
@@ -24,15 +20,23 @@ function DetailsScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Detail" component={DetailsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }}/>
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <MyTabs />
     </NavigationContainer>
   );
 }
