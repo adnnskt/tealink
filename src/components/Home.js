eabd25b4031a0e5d294798143da95fa310b5fd7c
Anderson Santos <adnnskt@gmail.com>
2023-04-18 20:55:13 -0300

    add card and style in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, StyleSheet} from 'react-native'
 
 
 export default props => {
 
-
+    const lado = 50 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+        <View style={style.flex}>
+            <View style={style.card}></View>
         </View>
+         
     )
 }
+
+
+const style = StyleSheet.create({
+    flex: {
+        flexGrow: 1,
+        width: '100%',
+        justifyContent: "space-between",
+        alignItems: 'center',
+    },
+    card: {
+        backgroundColor: "#000",
+        height: 50,
+        width: 50        
+    }
+})
