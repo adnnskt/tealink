07981a1fff07d9bedc089b898e08b0f6e9824c31
Anderson Santos <adnnskt@gmail.com>
2023-04-24 19:01:05 -0300

    work in home cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,29 +7,43 @@ export default props => {
     const lado = 50 
     return (
     <>  
-        <View style={style.flex}>
-            <View style={style.card}></View>
-            <View style={style.card}></View>
-            <View style={style.card}></View>
-        </View>
+        <View style={style.containner}>
+            <View style={style.flex}>
+                <View style={style.card}></View>
+                <View style={style.card}></View>
+                <View style={style.card}></View>
+            </View>
 
+            <View style={style.flex}>
+                <View style={style.card}></View>
+                <View style={style.card}></View>
+                <View style={style.card}></View>
+            </View>
+        </View>
     </>  
     )
 }
 
 
 const style = StyleSheet.create({
+    containner: {
+        //flexGrow: 1,
+        height: '100%',
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
     flex: {
         //flexGrow: 1,
         flexDirection: 'row',
-        height: '100%',
+        height: '50%',
         width: '100%',
-        justifyContent: "space-between",
+        justifyContent: "space-around",
         alignItems: 'center',
     },
     card: {
         backgroundColor: "#000",
-        height: 50,
-        width: 50        
+        height: '20%',
+        width: '30%'        
     }
 })
