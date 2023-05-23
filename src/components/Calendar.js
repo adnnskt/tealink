b6ec698c416408e07382bb3a482706975848d255
Anderson Santos <adnnskt@gmail.com>
2023-05-23 20:21:33 -0300

    ajust items in Agenda component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -13,10 +13,13 @@ export default props => {
         
         items={{
             '2023-05-22': [{name: 'item 1 - any js object'}],
-            '2023-05-23': [{name: 'item 2 - any js object', height: 80}],
-            '2023-05-25': [{name: 'item 3 - any js object'}, {name: 'any js object'}]
+            '2023-05-23': [{name: 'item 2 - any js object'}],
+            '2023-05-25': [{name: 'item 3 - any js object'}]
           }}
           
+          renderItem={(item, firstItemInDay) => {
+            return <Text>teste</Text>;
+          }}
 
         />
     )
