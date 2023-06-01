d3252ec5b8c9ceeef531ac2e9d9295070628d3d0
Anderson Santos <adnnskt@gmail.com>
2023-06-01 20:16:47 -0300

    try recreate home page

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { NativeBaseProvider, Text, Box, Center } from "native-base";
+import {View, StyleSheet, ImageBackground} from 'react-native'
+import Card from './Card'
+const backGround = require("../../src/img/background.png") 
+const hiperfoco = require("../../src/img/hiperfoco.png") 
+const crise = require("../../src/img/crise.png") 
+const introvertido = require("../../src/img/introvertido.png") 
+const terapia = require("../../src/img/terapia.png") 
+const desfralde = require("../../src/img/desfralde.png") 
+const remedio = require("../../src/img/remedio.png") 
 
 const LinearGradient = require('expo-linear-gradient').LinearGradient;
 
@@ -29,7 +38,13 @@ export default props => {
 
   return (
     <NativeBaseProvider config={config}>
-      <Center flex={1} px="3">
+      <Center flex={1} bg={{
+          linearGradient: {
+            colors: ['danger.200', 'danger.500'],
+            start: [0, 0],
+            end: [1.2, 0]
+          }
+        }}>
         <Component />
       </Center>
     </NativeBaseProvider>
