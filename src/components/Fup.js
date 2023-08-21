58c53500585a048707f5aeaa028afe9cd0d3acc8
Anderson Santos <adnnskt@gmail.com>
2023-08-21 19:22:24 -0300

    recrate content view with no UIlib

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -14,72 +14,16 @@ const LinearGradient = require('expo-linear-gradient').LinearGradient;
 
 export default props => {
 
-        return (  
-        <>
-            <View style={style.boxGroup}>
-                <View style={style.flex}>
-                    <TouchableHighlight style= {style.card} onPress = {()=> console.warn('teste')}>
-                        <Card image={crise} text='Crise'/>
-                    </TouchableHighlight>
-                    <TouchableHighlight style= {style.card} onPress = {null}>
-                        <Card image={hiperfoco} text='Hiperfoco'/>
-                    </TouchableHighlight>
-                    <TouchableHighlight style= {style.card} onPress = {null}>
-                        <Card image={introvertido} text='Introvertido'/>
-                    </TouchableHighlight>
-                </View>
+    return (  
+    <>
+        <View >
 
-            </View>
-            
-            <View style={style.boxGroup}>
-                <View style={style.flex}>
-                    <TouchableHighlight style= {style.card} onPress = {null}>   
-                        <Card image={terapia} text='Terapia'/>
-                    </TouchableHighlight>
-                    <TouchableHighlight style= {style.card} onPress = {null}>
-                        <Card image={desfralde} text='Desfralde'/>
-                    </TouchableHighlight>
-                    <TouchableHighlight style= {style.card} onPress = {null}>
-                        <Card image={remedio} text='Medicação'/>
-                    </TouchableHighlight>
-                </View>
-            </View>
-
-        </>  
+        </View>
+    </>  
     )
 }
       
 const style = StyleSheet.create({
    
-    boxGroup: {
-        height: '30%',
-        width: '100%',
-        //borderWidth: 3,
-        //borderColor: 'red',
-        backgroundColor: '#fff',
-        elevation: 5,
-    },
-    flex: {
-        //flexGrow: 1,
-        flexDirection: 'row',
-        height: '100%',
-        width: '100%',
-        justifyContent: "space-around",
-        alignItems: 'center',
-    },
-    card: {
-        //backgroundColor: "#fdf3f3",
-        height: '80%',
-        width: '30%',        
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        elevation: 10,
-        //boxShadow: '2%', 
-        //shadowColor: 'black',
-    },
-    image:{
-        height:'60%',
-        width: '100%',
-    }
+   
 })
