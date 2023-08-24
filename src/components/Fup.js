f761411f2583ad620d0a88d111f6ccd128571900
Anderson Santos <adnnskt@gmail.com>
2023-08-24 20:02:25 -0300

    import expo linear gradient

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -2,13 +2,7 @@ import React from "react";
 //import { NativeBaseProvider, Text, Box, Center } from "native-base";
 import {TouchableHighlight, Text, Image, View, StyleSheet, ImageBackground} from 'react-native'
 import Card from './Card'
-const backGround = require("../../src/img/background.png") 
-const hiperfoco = require("../../src/img/hiperfoco.png") 
-const crise = require("../../src/img/crise.png") 
-const introvertido = require("../../src/img/introvertido.png") 
-const terapia = require("../../src/img/terapia.png") 
-const desfralde = require("../../src/img/desfralde.png") 
-const remedio = require("../../src/img/remedio.png") 
+import { LinearGradient } from 'expo-linear-gradient';
 
 const LinearGradient = require('expo-linear-gradient').LinearGradient;
 
