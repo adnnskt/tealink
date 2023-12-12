497303c7769e4f3dc643de594c7b06f2e4084c34
Anderson Santos <adnnskt@gmail.com>
2023-12-12 19:53:13 -0300

    create json file

---
+++ src/views/criseContentJson.js
@@ -0,0 +1,16 @@
+const jsonList =  [
+    {
+        id: '1',
+        image: bgsensorial,
+        txt: 'Sensorial'
+    },
+    {
+        id: '2',
+        image: bgsensorial,
+        txt: 'Rotina'
+    }
+]
+
+
+
+export jsonList
