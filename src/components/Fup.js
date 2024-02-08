a4769fe9af59ca816b165db764005a8e16b793f5
Anderson Santos <adnnskt@gmail.com>
2024-02-08 18:21:40 -0300

    begin new component view test

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -1,1 +1,27 @@
-// work on img
+import React, {useState} from "react";
+import {TouchableOpacity, LayoutAnimation, NativeModules, TouchableHighlight, Text, Image, View, StyleSheet, ImageBackground, useWindowDimensions, ScrollView, Modal, Pressable, FlatList} from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import { Slider, RangeSlider } from '@react-native-assets/slider';
+
+const {UIManager} = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+
+export default props => {
+
+
+
+    return (
+    <>
+        <Text>text</Text>
+
+
+
+
+
+
+
+    </>
+    )
+}
