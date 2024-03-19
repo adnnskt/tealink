d45d19455cce0fa31dee1dc2661848cf7e5ad4c5
adnnskt <adnnskt@gmail.com>
2024-03-19 18:18:15 -0300

    add longTxt json data

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -16,27 +16,32 @@ export default props => {
         {
             id: '1',
             image: isolamento,
-            txt: 'Isolamento'
+            txt: 'Isolamento',
+            longTxt: "ao lidar com alguém dentro do espectro autista que enfrenta problemas de isolamento é criar um ambiente seguro e previsível. A consistência e a rotina podem ser fundamentais para promover a sensação de segurança. Tente manter um cronograma regular para as atividades diárias, fornecendo previsibilidade e reduzindo a ansiedade relacionada a mudanças repentinas. Além disso, é essencial entender e respeitar os limites pessoais da pessoa autista. Isso inclui respeitar suas necessidades de espaço e tempo para recarregar as energias. Não force interações sociais, mas esteja presente e disponível para quando a pessoa estiver pronta para se envolver. Encontre atividades que a pessoa autista goste e que promovam a interação social de uma maneira confortável para ela. Isso pode incluir hobbies específicos, jogos ou atividades sensoriais. Procure criar oportunidades para a conexão social de uma forma que respeite as preferências e limitações individuais. Por fim, esteja aberto à comunicação não verbal e desenvolva formas alternativas de se comunicar, como o uso de pictogramas, linguagem de sinais ou tecnologias assistivas, para facilitar a expressão e compreensão das necessidades e sentimentos da pessoa autista."
         },
         {
             id: '2',
             image: poucosAmigos,
-            txt: 'Poucos amigos'
+            txt: 'Poucos amigos',
+            longTxt: ""
         },
         {
             id: '3',
             image: vergonhaPublico,
-            txt: 'Vergonha em público'
+            txt: 'Vergonha em público',
+            longTxt: ""
         },
         {
             id: '4',
             image: cansadoDeFalar,
-            txt: 'Cansaço ao falar'
+            txt: 'Cansaço ao falar',
+            longTxt: ""
         },
         {
             id: '5',
             image: prefereMensagem,
-            txt: 'Prefere mensagem'
+            txt: 'Prefere mensagem',
+            longTxt: ""
         },
     ]
     
