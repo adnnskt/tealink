b16a7125218b544b8acfe42630029a84304e5225
adnnskt <adnnskt@gmail.com>
2024-03-21 18:39:38 -0300

    add "poucos amigos" txt

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -23,7 +23,7 @@ export default props => {
             id: '2',
             image: poucosAmigos,
             txt: 'Poucos amigos',
-            longTxt: ""
+            longTxt: "Uma dica essencial ao cuidar de alguém no espectro autista com poucos amigos é incentivar a participação em atividades que facilitem a formação de novas amizades, como grupos relacionados aos seus interesses. Apoiar a prática de habilidades sociais, através de jogos de role-playing ou atividades estruturadas, é fundamental para aumentar a confiança e a habilidade da pessoa em interagir com os outros, promovendo assim um maior envolvimento social e a possibilidade de estabelecer novas conexões."
         },
         {
             id: '3',
