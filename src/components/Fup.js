89c58197425ccb6b5f8ad90807d772e9589779a6
adnnskt <adnnskt@gmail.com>
2024-03-29 16:30:06 -0300

    start new view

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -2,3 +2,18 @@ import React, {useState} from "react";
 import { Text, Image, View, StyleSheet,  useWindowDimensions, FlatList} from 'react-native'
 //import { LinearGradient } from 'expo-linear-gradient'
 //import { Slider, RangeSlider } from '@react-native-assets/slider';
+
+
+export default props => {
+
+
+
+
+
+    return(
+        <>
+            
+        
+        </>
+    )
+}
