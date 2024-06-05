669cef8f52e52ce20b690ca63cff3a413152a661
adnnskt <adnnskt@gmail.com>
2024-06-05 18:31:57 -0300

    import images

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -2,6 +2,10 @@ import React, {useState} from "react";
 import { Text, Image, View, StyleSheet,  Pressable, ScrollView, Modal, Switch} from 'react-native'
 import { FontAwesome } from '@expo/vector-icons';
 
+const menino = require("../../src/img/meninoBanheiro.jpg") 
+const meninoDormindo = require("../../src/img/meninoDormindo.jpg") 
+
+
 export default props => {
 
     const [isEnabled, setIsEnabled] = useState(false);
