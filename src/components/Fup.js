d8382d8272b8a86e12c272e2695c58db80e9f845
adnnskt <adnnskt@gmail.com>
2024-07-16 17:09:27 -0300

    start firts configs

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -2,3 +2,30 @@ import React, {useState} from "react";
 import { LinearGradient } from 'expo-linear-gradient'
 import {TouchableOpacity, LayoutAnimation, NativeModules, TouchableHighlight, Text, Image, View, StyleSheet, ImageBackground, useWindowDimensions, ScrollView, Modal, Pressable, FlatList} from 'react-native'
 
+
+export default props => {
+
+
+
+
+
+    return (
+        <>
+            <View style={style.container}>
+
+            </View>
+
+        </>
+    )
+}
+
+const style = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#f5cee7',
+        justifyContent: 'space-around',
+    },
+
+
+})
