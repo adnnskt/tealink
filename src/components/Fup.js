4245b33226edb79c61bb9681c11359decd2c3acd
adnnskt <adnnskt@gmail.com>
2024-11-13 18:29:01 -0300

    start refactory home page

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, FlatList, StyleSheet, Button, Pressable } from 'react-native';
+const logo = require("../../src/img/logo.png") 
+
 
 export default function App() {
  
 
   return (
     <View style={styles.container}>
-      
+      <View style = {style.logo}>
+            <Image
+              source={logo} 
+              resizeMode="cover"
+              style={style.image}>    
+            </Image>
+      </View>
     </View>
   );
 }
