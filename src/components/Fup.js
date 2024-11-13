aa6ff6104038b332e072dd9134d38fcb99985fad
adnnskt <adnnskt@gmail.com>
2024-11-13 18:45:47 -0300

    fix logo justify

--- src/components/Fup.js
+++ src/components/Fup.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, FlatList, StyleSheet, Button, Pressable } from 'react-native';
+import { Text, View, FlatList, StyleSheet, Button, Pressable, Image } from 'react-native';
 const logo = require("../../src/img/logo.png") 
 
 
@@ -8,12 +8,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style = {style.logo}>
+      <View style = {styles.logo}>
+        <View style = {styles.underLogo}>
             <Image
               source={logo} 
-              resizeMode="cover"
-              style={style.image}>    
+              resizeMode= "contain"//"cover"
+              style={styles.image}>    
             </Image>
+        </View>
       </View>
     </View>
   );
@@ -24,6 +26,29 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: '#fff',
+  },
+  logo: {
+    height:'20%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'#fff',
+    //paddingTop: '5%',
+    //borderWidth: '3',
+    //borderColor: 'blue',
+  },
+  underLogo: {
+    height:'100%',
+    width: '90%',   
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 3,
+    borderColor: 'blue',
+  },
+  image: {
+    height:'80%',
+    width: '100%',
   },
   
 });
